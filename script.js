337617b8a721551280b1fee804cd13e85f058fcb
Prem B. Shah <techie.prembshah@gmail.com>
2024-02-08 17:05:20 +0530

    Minor modifications and bug-fixes with the cart button

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ async function render() {
                     <h2>${product.title}</h2>
                 </span>
                 <div class="like">
-                    <button id="liked-${product.id}" class="like-btn">
+                    <button id="like-${product.id}" class="like-btn" onclick="toggleLike(${product.id})">
                         <i class="fa-${likeClass} fa-heart fa-xl"></i>
                     </button>
                 </div>
@@ -42,9 +42,9 @@ async function render() {
         </div>
     </div>`;
   });
+}
 
-
-  const cart = document.querySelector(".cart-btn");
+const cart = document.querySelector(".cart-btn");
   cart.addEventListener("click", () => {
     if(cartClicked){
         render();
@@ -54,40 +54,18 @@ async function render() {
     }
     cartClicked = !cartClicked;
   });
-  function toggleLike(productId) {
-    const likeBtn = document.getElementById(`liked-${productId}`);
-    const isLiked = JSON.parse(localStorage.getItem(`liked-${productId}`));
-  
-    if (isLiked) {
-      localStorage.removeItem(`liked-${productId}`);
-      likeBtn.firstElementChild.classList.remove("fa-solid");
-      likeBtn.firstElementChild.classList.add("fa-regular");
-    } else {
-      localStorage.setItem(`liked-${productId}`, true);
-      likeBtn.lastElementChild.classList.remove("fa-regular");
-      likeBtn.lastElementChild.classList.add("fa-solid");
-    }
-  }
-  const likeButtons = document.querySelectorAll(".like-btn");
-  likeButtons.forEach((likeBtn) => {
-    likeBtn.addEventListener("click", () => {
-      var id = likeBtn.getAttribute("id").split("-")[1];
-      toggleLike(id);
-    });
-  });
-
-}
 
 async function displayLikedProducts() {
-  const items = document.querySelector(".items");
-  items.innerHTML = "";
-
-  const products = await getData(url);
-
-  products.forEach((product) => {
-    const isLiked = JSON.parse(localStorage.getItem(`liked-${product.id}`) || "false");
-    if (isLiked) {
-      items.innerHTML += `<div class="product">
+    const items = document.querySelector(".items");
+    items.innerHTML = "";
+  
+    const products = await getData(url);
+    const liked = [];
+  
+    products.forEach((product) => {
+      const isLiked = JSON.parse(localStorage.getItem(`liked-${product.id}`) || "false");
+      if (isLiked) {
+        items.innerHTML += `<div class="product">
             <div class="product-img">
                 <img id="${product.id}" src="${product.image}" alt="product">
             </div>
@@ -108,9 +86,16 @@ async function displayLikedProducts() {
                 </div>
             </div>
         </div>`;
+        liked.push(product);
+      }
+    });
+    if(liked.length === 0){
+        items.innerHTML += `<div class="not-found">
+        <span> <i class="fa-solid fa-triangle-exclamation fa-xl"></i> Your liked products will get displayed here .... No liked products ... !</span>
+    </div>`;
     }
-  });
-}
+  }
+  
 
 function toggleLike(productId) {
     const likeBtn = document.getElementById(`like-${productId}`);
@@ -158,7 +143,7 @@ async function search() {
                             <h2>${product.title}</h2>
                         </span>
                         <div class="like">
-                            <button id="like-${product.id}" class="like-btn" onclick="toggleLike(${product.id})"> <!-- Call toggleLike function when clicked -->
+                            <button id="like-${product.id}" class="like-btn" onclick="toggleLike(${product.id})">
                                 <i class="fa-${likeClass} fa-heart fa-xl"></i>
                             </button>
                         </div>
